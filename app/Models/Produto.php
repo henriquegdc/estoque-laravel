6f96ae3035e60029867a2b26e5e6bdb047849175
Henrique Carvalho <henrique.carvalho1220@gmail.com>
2026-09-11 20:00:33 -0300

    feat: define fillable e casts no model Produtos

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $fillable = ['nome', 'sku','preco','quantidade', 'ativo'];
+    protected $casts = [
+        'preco' => 'decimal:2',
+        'ativo' => 'boolean'
+    ];
 }
