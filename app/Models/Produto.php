2a5d94398fd0d07445cc8ee6495255ad68ca3bdc
Henrique Carvalho <henrique.carvalho1220@gmail.com>
2026-09-15 00:00:28 -0300

    feat: relaciona produto e categoria com hasMany e belongsTo

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,9 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    protected $fillable = ['nome', 'sku','preco','quantidade', 'ativo'];
+    protected $fillable = ['nome', 'sku','preco','quantidade', 'ativo', 'categoria_id'];
     protected $casts = [
         'preco' => 'decimal:2',
         'ativo' => 'boolean'
     ];
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
 }
